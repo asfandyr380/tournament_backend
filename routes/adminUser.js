7cb8248bca4920d906d7d8f70b7a98d45544f62e
Asfand Yr <asfandyr380@gmail.com>
2020-07-13 09:07:59 -0700

    Improvements in adminUser Code

--- routes/adminUser.js
+++ routes/adminUser.js
@@ -4,13 +4,11 @@ const bcrypt = require('bcrypt');
 const adminModel = require('../Models/adminUser_model');
 
 
-
-
-route.post('/register', async (req,res) => {
+route.post('/register', async (req, res) => {
 
     // Check if user Already Exits
-    const userExits = await adminModel.findOne({username: req.body.username});
-    if(userExits) return res.status(400).send('user Already Exits');
+    const userExits = await adminModel.findOne({ username: req.body.username });
+    if (userExits) return res.status(400).send('user Already Exits');
 
     // Hash The Password For Security
     const salt = await bcrypt.genSalt(10);
@@ -22,33 +20,32 @@ route.post('/register', async (req,res) => {
         username: username,
         password: hashedPass,
     });
-    try{
-        if (username == "" || req.body.password == "")
-        {
+    try {
+        if (username == "" || req.body.password == "") {
             res.status(500).send('Invalid Input By User');
-        }else {
+        } else {
             const registerUser = await newUser.save();
             res.status(201).json(registerUser);
         }
-    }catch(err)
-    {
-        res.status(400).json({message: err.message});
+    } catch (err) {
+        res.status(400).json({ message: err.message });
     }
 });
 
 
 
-route.post('/logIn', async (req,res) => {
+route.post('/logIn', async (req, res) => {
 
     // login user if it Exits
-    const userexits = await adminModel.findOne({username: req.body.username});
+    const userexits = await adminModel.findOne({ username: req.body.username });
     if (!userexits) return res.status(400).send('Invalid Username');
 
     // Validate The Password
     const validPass = await bcrypt.compare(req.body.password, userexits.password);
-    if(!validPass) return res.status(400).send('Invalid Password');
+    if (!validPass) return res.status(400).send('Invalid Password');
 
-    res.status(200).send('Login Successful');
+    const jUser = await adminModel.find({username: req.body.username});
+    res.status(200).json(jUser);
 
 });
 
